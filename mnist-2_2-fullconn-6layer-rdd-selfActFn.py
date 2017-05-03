0c70ef8f1debc620062fa55a5fb3ba7b190e16a7
wmji <maoji.wang@cims.nyu.edu>
2017-05-03 13:17:23 -0400

    add self-def actavation func, and conv2d network

---
+++ mnist-2_2-fullconn-6layer-rdd-selfActFn.py
@@ -0,0 +1,180 @@
+#  !/usr/bin/env python
+#  -*- coding:utf-8 -*-
+
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# https://www.tensorflow.org/get_started/mnist/beginners
+# https://codelabs.developers.google.com/codelabs/cloud-tensorflow-mnist/
+# http://stackoverflow.com/questions/39921607/\
+#   tensorflow-how-to-make-a-custom-activation-function-with-only-python
+# wmji@github.com
+
+# this codes shows how to use user-defined activation function
+# define a good activation function
+# or, accept a bad result, as shown in this code.
+
+import tensorflow as tf
+from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
+from tensorflow.python.framework import ops
+tf.set_random_seed(0)  # tf&DL_without_a_phD
+
+
+# self-def activation function:
+def triple(x): return x**3/3.0  # this is the activation function
+np_triple = np.vectorize(triple)   # make it a numpy function
+
+def d_triple(x): return 1.0* x**2  # gradient of the activation
+np_d_triple = np.vectorize(d_triple)
+
+### make a numpy function to a tensorflow function
+### tf.py_func(func, inp, Tout, stateful=stateful, name=name) do transformation
+###    https://www.tensorflow.org/api_docs/python/tf/py_func
+np_d_triple_32 = lambda x: np_d_triple(x).astype(np.float32)
+
+def tf_d_triple(x,name=None):
+    with ops.name_scope(name, "d_triple", [x]) as name:
+        y = tf.py_func(np_d_triple_32,
+                        [x],
+                        [tf.float32],
+                        name=name,
+                        stateful=False)
+        return y[0]
+
+### hack to get gradients
+def py_func(func, inp, Tout, stateful=True, name=None, grad=None):
+    # Need to generate a unique name to avoid duplicates:
+    rnd_name = 'PyFuncGrad' + str(np.random.randint(0, 1E+10))
+    # ...
+    tf.RegisterGradient(rnd_name)(grad)  # see _MySquareGrad for grad example
+    g = tf.get_default_graph()
+    with g.gradient_override_map({"PyFunc": rnd_name}):
+        return tf.py_func(func, inp, Tout, stateful=stateful, name=name)
+
+def triple_grad(op, grad):
+    x = op.inputs[0]
+    # ...
+    n_gr = tf_d_triple(x)
+    return grad * n_gr
+
+### combining it all together
+np_triple_32 = lambda x: np_triple(x).astype(np.float32)
+# ...
+def tf_triple(x, name=None):
+    # ...
+    with ops.name_scope(name, "triple", [x]) as name:
+        y = py_func(np_triple_32,
+                        [x],
+                        [tf.float32],
+                        name=name,
+                        grad=triple_grad)  # <-- here's the call to the gradient
+        return y[0]
+
+
+# data
+mnist = read_data_sets("MNIST_data/", one_hot=True)
+print("MNIST data ready for analysis!\n")  # get data ready
+batch_size = 100  # how many imgs in each batch?
+
+
+# model
+# neural network with 6 layers of 400,200,100,50,25,10 neurons, respectively
+# this is similar with mnist_1layer.py, only that a few sigmoid layers added
+# UPDATE:
+# 1 use self-defined transfer function, instead of sigmoid/relu
+nn0,nn1,nn2,nn3,nn4,nn5,nn6 = 784,400,200,100,50,25,10  # neuroNumber each layer
+x = tf.placeholder(tf.float32, [None, nn0]) # for inputing imgs, be of batchSize
+transFunc = tf_triple # tf.nn.relu  # tf.nn.sigmoid
+# Probability of keeping a node during dropout
+#  =1 at test time (no dropout) and ~0.7 at training time.
+pkeep = tf.placeholder(tf.float32)
+
+W1 = tf.Variable(tf.truncated_normal([nn0, nn1], stddev=0.1)) #784=28*28, inputs
+b1 = tf.Variable(tf.zeros([nn1])+0.01)
+y1 = transFunc(tf.matmul(x, W1) + b1)
+y1d = tf.nn.dropout(y1, pkeep)
+
+W2 = tf.Variable(tf.truncated_normal([nn1, nn2], stddev=0.1))
+b2 = tf.Variable(tf.zeros([nn2])+0.01)
+y2 = transFunc(tf.matmul(y1d, W2) + b2)
+y2d = tf.nn.dropout(y2, pkeep)
+
+W3 = tf.Variable(tf.truncated_normal([nn2, nn3], stddev=0.1))
+b3 = tf.Variable(tf.zeros([nn3])+0.01)
+y3 = transFunc(tf.matmul(y2d, W3) + b3)
+y3d = tf.nn.dropout(y3, pkeep)
+
+W4 = tf.Variable(tf.truncated_normal([nn3, nn4], stddev=0.1))
+b4 = tf.Variable(tf.zeros([nn4])+0.01)
+y4 = transFunc(tf.matmul(y3d, W4) + b4)
+y4d = tf.nn.dropout(y4, pkeep)
+
+W5 = tf.Variable(tf.truncated_normal([nn4, nn5], stddev=0.1))
+b5 = tf.Variable(tf.zeros([nn5])+0.01)
+y5 = transFunc(tf.matmul(y4d, W5) + b5)
+y5d = tf.nn.dropout(y5, pkeep)
+
+W6 = tf.Variable(tf.truncated_normal([nn5, nn6], stddev=0.1))
+b6 = tf.Variable(tf.zeros([nn6])+0.01)
+yLogits = tf.matmul(y5d, W6) + b6
+y = tf.nn.softmax(yLogits)
+
+y_ = tf.placeholder(tf.float32, [None, nn6])  # will be loaded in sess.run()
+
+
+# loss
+# cross-entropy loss function (= -sum(Y_i * log(Yi)) )
+#    normalised for batches of 100 images
+# use softmax_cross_entropy_with_logits to avoid numerical stability problems,
+# ie, log(0) is NaN
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits\
+                      (logits=yLogits, labels=y_))*100
+
+
+# training
+# learning rate decay
+max_learning_rate = 0.003
+min_learning_rate = 0.0001
+decay_speed = 2000.0 # 0.003-0.0001-2000=>0.9826 done in 5000 iterations
+# variable learning rate
+lr = tf.placeholder(tf.float32)
+# the optimizer
+train_stepper = tf.train.AdamOptimizer(lr).minimize(loss)
+
+
+# initializer
+init = tf.global_variables_initializer()  # note the version problem
+
+
+# evaluation
+# arg_max : the entry with the highest probability is our prediction
+if_prediction_correct = tf.equal(tf.arg_max(y, 1), tf.arg_max(y_, 1)) # T,F,T...
+accuracy = tf.reduce_mean(tf.cast(if_prediction_correct, tf.float32)) # 1,0,1...
+
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    # training
+    for i in range(2000):  # train_step_number
+        # learning rate decay
+        lerate = min_learning_rate + \
+                (max_learning_rate-min_learning_rate) * math.exp(-i/decay_speed)
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size) # load & train:
+        sess.run(train_stepper, {x:batch_xs, y_:batch_ys, pkeep:0.7, lr:lerate})
+        if i % 100 == 0:
+            train_accuacy = accuracy.eval(feed_dict={x: batch_xs, y_: batch_ys, pkeep: 1.0})
+            print("step %d, training accuracy %g"%(i, train_accuacy))
+
+    # accuacy on test
+    print("test accuracy %g"%(accuracy.eval(\
+           feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})))
+    print("Accuarcy on Test-dataset: ", sess.run(accuracy, \
+        feed_dict={x:mnist.test.images, y_:mnist.test.labels, pkeep:1}))
+
+
+print("\nDone.")  # all done
---
+++ mnist-3_0-conv2d.py
@@ -0,0 +1,112 @@
+#  !/usr/bin/env python
+#  -*- coding:utf-8 -*-
+
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# https://www.tensorflow.org/get_started/mnist/beginners
+# https://codelabs.developers.google.com/codelabs/cloud-tensorflow-mnist/
+# wmji@github.com
+
+
+import tensorflow as tf
+from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
+tf.set_random_seed(0)  # tf&DL_without_a_phD
+
+def weight_varible(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+
+# data
+mnist = read_data_sets("MNIST_data/", one_hot=True)
+print("MNIST data ready for analysis!\n")  # get data ready
+batch_size = 100  # how many imgs in each batch?
+
+
+# model
+# · · · · · · · · · ·      (input data, 1-deep)                 X [batch, 28, 28, 1]
+# @ @ @ @ @ @ @ @ @ @   -- conv. layer 5x5x1=>4 stride 1        W1 [5, 5, 1, 4]        B1 [4]
+# ∶∶∶∶∶∶∶∶∶∶∶∶∶∶∶∶∶∶∶                                           Y1 [batch, 28, 28, 4]
+#   @ @ @ @ @ @ @ @     -- conv. layer 5x5x4=>8 stride 2        W2 [5, 5, 4, 8]        B2 [8]
+#   ∶∶∶∶∶∶∶∶∶∶∶∶∶∶∶                                             Y2 [batch, 14, 14, 8]
+#     @ @ @ @ @ @       -- conv. layer 4x4x8=>12 stride 2       W3 [4, 4, 8, 12]       B3 [12]
+#     ∶∶∶∶∶∶∶∶∶∶∶                                               Y3 [batch, 7, 7, 12] => reshaped to YY [batch, 7*7*12]
+#      \x/x\x\x/        -- fully connected layer (relu)         W4 [7*7*12, 200]       B4 [200]
+#       · · · ·                                                 Y4 [batch, 200]
+#       \x/x\x/         -- fully connected layer (softmax)      W5 [200, 10]           B5 [10]
+#        · · ·                                                  Y [batch, 20]
+sess = tf.InteractiveSession()
+
+# paras
+W_conv1 = weight_varible([5, 5, 1, 32])
+b_conv1 = bias_variable([32])
+
+# conv layer-1
+x = tf.placeholder(tf.float32, [None, 784])
+x_image = tf.reshape(x, [-1, 28, 28, 1])
+
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+h_pool1 = max_pool_2x2(h_conv1)
+
+# conv layer-2
+W_conv2 = weight_varible([5, 5, 32, 64])
+b_conv2 = bias_variable([64])
+
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
+
+# full connection
+W_fc1 = weight_varible([7 * 7 * 64, 1024])
+b_fc1 = bias_variable([1024])
+
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+# dropout
+keep_prob = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+# output layer: softmax
+W_fc2 = weight_varible([1024, 10])
+b_fc2 = bias_variable([10])
+
+y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+y_ = tf.placeholder(tf.float32, [None, 10])
+
+# model training
+cross_entropy = -tf.reduce_sum(y_ * tf.log(y_conv))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+
+correct_prediction = tf.equal(tf.arg_max(y_conv, 1), tf.arg_max(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+
+    for i in range(2000):
+        batch = mnist.train.next_batch(50)
+
+        if i % 100 == 0:
+            train_accuacy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
+            print("step %d, training accuracy %g"%(i, train_accuacy))
+        train_step.run(feed_dict = {x: batch[0], y_: batch[1], keep_prob: 0.5})
+
+    # accuacy on test
+    print("test accuracy %g"%(accuracy.eval(\
+           feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})))
